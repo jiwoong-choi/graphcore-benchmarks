bdda12b4202256c479fda432b72a43f3a0eb4468
Jiwoong Choi <jiwoongc@graphcore.ai>
2022-07-22 14:20:15 +0900

    first commit

---
+++ data/imagenet/download_imagenet.sh
@@ -0,0 +1,97 @@
+#! /bin/bash
+if [[ "$#" -gt 1 ||  "$#" == 0 ]]; then
+  echo "Usage: $0 DIRECTORY_TO_DOWNLOAD_IMAGENET"
+  exit
+fi
+
+DATASET_DIR=$1
+if [ ! -d ${DATASET_DIR} ]; then
+  echo "Error: ${DATASET_DIR}: no such directory"
+  exit
+fi
+
+TRAINING_FILE="${DATASET_DIR}/ILSVRC2012_img_train.tar"
+VALIDATION_FILE="${DATASET_DIR}/ILSVRC2012_img_val.tar"
+TRAIN_DIR="${DATASET_DIR}/train"
+VAL_DIR="${DATASET_DIR}/val"
+
+function download-from-link () {
+  local FILE_PATH=$1
+  local URL=$2
+  local EXPECTED_FILE_SIZE=$3
+  if [ ! -f ${FILE_PATH} ]; then
+    echo "Downloading ${FILE_PATH} ..."
+    wget -O ${FILE_PATH} ${URL}
+  else
+    local FILE_SIZE="$(ls -l ${FILE_PATH} | awk '{print $5}')"
+    if [[ ${FILE_SIZE} == ${EXPECTED_FILE_SIZE} ]]; then
+      echo "${FILE_PATH} already downloaded."
+    else
+      echo "${FILE_PATH} is found, but not complete. (expected size was ${EXPECTED_FILE_SIZE}, but got ${FILE_SIZE})"
+      echo "Continuing to download ${FILE_PATH} ..."
+      wget -cO ${FILE_PATH} ${URL}
+    fi
+  fi
+}
+
+function unpack-training-file () {
+  mkdir -p ${TRAIN_DIR}
+  cd ${TRAIN_DIR}
+  echo "Unpacking ${TRAINING_FILE} ..."
+  tar -xvf "${TRAINING_FILE}" &> /dev/null
+  ls *.tar | while read NAME; do mkdir -p "${NAME%.tar}"; cd "${NAME%.tar}"; echo "Unpacking ${NAME} ..."; tar -xvf ../$NAME &> /dev/null; cd ..; rm $NAME; done
+  cd ${DATASET_DIR}
+}
+
+function unpack-validation-file () {
+  mkdir -p ${VAL_DIR}
+  cd ${VAL_DIR}
+  echo "Unpacking ${VALIDATION_FILE} ..."
+  tar -xvf "${VALIDATION_FILE}" &> /dev/null
+  wget -qO- https://raw.githubusercontent.com/soumith/imagenetloader.torch/master/valprep.sh | bash
+  cd ${DATASET_DIR}
+}
+
+download-from-link ${TRAINING_FILE} "https://image-net.org/data/ILSVRC/2012/ILSVRC2012_img_train.tar" 147897477120
+download-from-link ${VALIDATION_FILE} "https://image-net.org/data/ILSVRC/2012/ILSVRC2012_img_val.tar" 6744924160
+
+if [ ! -d ${TRAIN_DIR} ]; then
+  unpack-training-file
+else
+  TRAIN_DIR_SIZE="$(du --max-depth 0 ${TRAIN_DIR})"
+  if [[ ${TRAIN_DIR_SIZE} == 146087396 ]]; then
+    echo "${TRAINING_FILE} already unpacked."
+  else
+    echo "${TRAIN_DIR} is found, but not complete. (expected size was 146087396, but got ${TRAIN_DIR_SIZE})"
+    rm -rf ${TRAIN_DIR}
+    unpack-training-file
+  fi
+fi
+
+if [ ! -d ${VAL_DIR} ]; then
+  unpack-validation-file
+else
+  VAL_DIR_SIZE="$(du --max-depth 0 ${VAL_DIR})"
+  if [[ ${VAL_DIR_SIZE} == 6655288 ]]; then
+    echo "${VALIDATION_FILE} already unpacked."
+  else
+    echo "${VAL_DIR} is found, but not complete. (expected size was 6655288, but got ${VAL_DIR_SIZE})"
+    rm -rf ${VAL_DIR}
+    unpack-validation-file
+  fi
+fi
+
+BBOX_INFO_PATH="${DATASET_DIR}/imagenet_2012_bounding_boxes.csv"
+TFRECORD_DIR="${DATASET_DIR}/tfrecord"
+download-from-link ${BBOX_INFO_PATH} https://repository.prace-ri.eu/git/Data-Analytics/Benchmarks/-/raw/bfea8c2c69078a98c35a6e774809e7d9cc807874/ImageNetUseCaseV2/Dataset/Metadata/imagenet_2012_bounding_boxes.csv?inline=false 29709928
+virtualenv -p python3.6 venv
+source venv/bin/activate
+pip install tensorflow==1.15.2
+mkdir -p ${TFRECORD_DIR}
+python imagenet-to-tfrecord.py \
+  --bounding_box_file ${BBOX_INFO_PATH} \
+  --imagenet_metadata_file imagenet_metadata.txt \
+  --labels_file imagenet_lsvrc_2015_synsets.txt \
+  --output_directory ${TFRECORD_DIR} \
+  --train_directory ${TRAIN_DIR} \
+  --validation_directory ${VAL_DIR}
---
+++ mlperf-resnet50-pod16.sh
@@ -0,0 +1,75 @@
+#! /bin/bash
+if [[ "$#" -gt 1 ||  "$#" == 0 ]]; then
+  echo "Usage: $0 IMAGENET_TFRECORD_DIR"
+  exit
+fi
+
+DATA_DIR=$1
+WORKING_DIR=`pwd`
+TARGET_POPLAR_SDK_VERSION="2.5.1+1001"
+POPLAR_SDK_VERSION="$(python3 -c 'import json; import os; print(json.load(open(os.path.join(os.environ["POPLAR_SDK_ENABLED"], "..", "manifest.json")))["details"]["version"])')"
+TRAINING_DIR="${WORKING_DIR}/training_results_v2.0/Graphcore/benchmarks/resnet/implementations/tensorflow"
+
+if [ -z ${POPLAR_SDK_VERSION} ]; then
+  echo "Please activate a Poplar SDK."
+  exit
+else
+  if [ ${POPLAR_SDK_VERSION} != ${TARGET_POPLAR_SDK_VERSION} ]; then
+    echo "Warning: This script was tested with Poplar SDK ${TARGET_POPLAR_SDK_VERSION}, but you're currently using ${POPLAR_SDK_VERSION}."
+  fi
+fi
+
+if [ ! -d "${WORKING_DIR}/training_results_v2.0" ]; then
+  git clone https://github.com/mlcommons/training_results_v2.0 ${WORKING_DIR}/training_results_v2.0 &> /dev/null
+fi
+
+ENV_PATH="${WORKING_DIR}/venv-${POPLAR_SDK_VERSION}"
+if [ ! -d $ENV_PATH ]; then
+   echo "Setting up Python3 virtual environment at $ENV_PATH"
+   virtualenv -p python3 $ENV_PATH &> /dev/null
+   source "$ENV_PATH/bin/activate"
+
+   echo "Installing Tensorflow 1 ..."
+   pip install $POPLAR_SDK_ENABLED/../tensorflow-1*amd*.whl &>> "$ENV_PATH/install.log"
+
+   echo "Installing WandB ..."
+   pip install wandb &>> $ENV_PATH/install.log
+
+   echo "Installing required packages for Tensorflow 1 CNNs ..."
+   cd $TRAINING_DIR
+   pip install -r requirements.txt &>> $ENV_PATH/install.log
+   cd $WORKING_DIR
+else
+   echo "Skipping virtualenv setup. $ENV_PATH directory already exists"
+   source "$ENV_PATH/bin/activate"
+fi
+
+cd $TRAINING_DIR
+
+for ((n=1; n<5; n++)); do
+  SEED=$((n << 32));
+  TIMESTAMP=$(date +%Y-%m-%d_%H-%M-%S)
+  LOG_DIR="${WORKING_DIR}/logs/${TIMESTAMP}"
+  mkdir -p ${LOG_DIR}
+  OUTPUT_FILE="${WORKING_DIR}/logs/${TIMESTAMP}/output.log"
+
+  echo "Running ResNet50 training with seed ${SEED} ..."
+  echo "(To watch the progress, run 'tail -f ${LOG_DIR}/log.txt' on a new terminal)"
+  POPLAR_ENGINE_OPTIONS='{"target.hostSyncTimeout":"900"}' \
+  POPLAR_RUNTIME_OPTIONS='{"streamCallbacks.maxLookahead":"unlimited"}' \
+  poprun \
+    --mpi-global-args='--tag-output --allow-run-as-root' \
+    --mpi-local-args='-x POPLAR_ENGINE_OPTIONS -x POPLAR_RUNTIME_OPTIONS' \
+    --ipus-per-replica 1 --numa-aware 1 \
+    --num-instances 8 --num-replicas 16 \
+  python -u train.py \
+    --config mk2_resnet50_mlperf_pod16_lars \
+    --seed ${SEED} \
+    --identical-replica-seeding \
+    --stochastic-rounding ON \
+    --logs-path ${LOG_DIR} \
+    --data-dir ${DATA_DIR} \
+  &> ${OUTPUT_FILE}
+done
+
+echo "Finished benchmarking."
